3aaef31cfafec5cad45eeb519fcee6c65c5c55d3
Gustavo Meneses Gois <gustavo.gois@gmail.com>
2022-12-22 22:00:05 +0000

    3. Creating a Set of Cooperating Microservices

--- src/main/java/com/gois/microservices/core/product/review/ReviewServiceApplication.java
+++ src/main/java/com/gois/microservices/core/product/review/ReviewServiceApplication.java
@@ -1,9 +1,11 @@
-package com.gois.reviewservice;
+package com.gois.microservices.core.product.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.gois")
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
